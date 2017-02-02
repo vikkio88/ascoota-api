ee26c54c72df5e883e305fd71ea16a13a1f424c7
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-02 22:06:05 +0000

    fixed new line problem when script was called from webbrowser

--- scripts/includes/includes.php
+++ scripts/includes/includes.php
@@ -1,7 +1,12 @@
 <?php
 require '../vendor/autoload.php';
 
+function isConsole()
+{
+    return php_sapi_name() == "cli";
+}
+
 function logInfo($message)
 {
-    echo '-- ' . (new DateTime())->format('H:i:s d-m-Y') . '  --- ' . $message . PHP_EOL;
+    echo '-- ' . (new DateTime())->format('H:i:s d-m-Y') . '  --- ' . $message . PHP_EOL . (!isConsole() ? '<br />' : '');
 }
