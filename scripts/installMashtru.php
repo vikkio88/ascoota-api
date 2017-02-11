cd8df50696a8f6bda89f6036ef247cf4b6b561a8
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-11 16:36:22 +0000

    adding visual aid on execution

--- scripts/installMashtru.php
+++ scripts/installMashtru.php
@@ -24,7 +24,7 @@ $jobDb = JobEntityFactory::getInstance(
     JobManager::TABLE_NAME
 );
 
-echo "Creating db structure" . PHP_EOL;
+echo "<pre>Creating db structure" . PHP_EOL . PHP_EOL;
 $jobDb->uninstall();
 $jobDb->install();
 
@@ -33,5 +33,15 @@ foreach ($jobsConfig as $job) {
     $job['fire_time'] = Carbon::now()->format(JobEntity::TIME_FORMAT);
     $jobDb->create($job);
 }
-echo "Installed successfully" . PHP_EOL;
+
+echo <<<EOF
+                  _     _              _      _           _        _ _          _ 
+  /\/\   __ _ ___| |__ | |_ _ __ _   _( )__  (_)_ __  ___| |_ __ _| | | ___  __| |
+ /    \ / _` / __| '_ \| __| '__| | | |/ __| | | '_ \/ __| __/ _` | | |/ _ \/ _` |
+/ /\/\ \ (_| \__ \ | | | |_| |  | |_| |\__ \ | | | | \__ \ || (_| | | |  __/ (_| |
+\/    \/\__,_|___/_| |_|\__|_|   \__,_||___/ |_|_| |_|___/\__\__,_|_|_|\___|\__,_|
+                                                                                  
+EOF;
+
+echo "</pre>";
 unlink(__FILE__);
