8050f9d97f782a723efab52f0317819916c3d672
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-11 16:05:35 +0000

    adding mashtru configurations

---
+++ config/cron.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'jobs' => [
+        [
+            'name' => 'fixPodcastLinks',
+            'class_name' => 'FixPodcastLinks',
+            'args' => null,
+            'delta_minutes' => 60 * 24,
+            'active' => true,
+            'fire_time' => null,
+            'priority' => 0
+        ],
+        [
+            'name' => '',
+            'class_name' => 'FetchNewPodcasts',
+            'args' => null,
+            'delta_minutes' => 60 * 24,
+            'active' => true,
+            'fire_time' => null,
+            'priority' => 1
+        ]
+    ],
+    'namespaces' => [
+
+    ]
+];
