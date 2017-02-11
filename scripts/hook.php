9fc374cbc9fec66519e24e5ea8cfeef8006e8047
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-11 16:06:17 +0000

    created mashtru hook

---
+++ scripts/hook.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use App\Lib\Helpers\Config;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Mashtru\JobManager;
+use Mashtru\Libs\Helpers\DBConfig;
+use Mashtru\Libs\Helpers\RunnerConfig;
+
+$dbConfig = Capsule::connection()->getConfig();
+
+$jobs = new JobManager(
+    new DBConfig(
+        $dbConfig['host'],
+        $dbConfig['database'],
+        $dbConfig['username'],
+        $dbConfig['password']
+    ),
+    new RunnerConfig(
+        Config::get('cron.namespaces')
+    )
+);
+
+
+$jobs->fire();
+
+
+
+
