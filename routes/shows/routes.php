6e295c9d8b3b1306c11bcd54b840037f47a9dd3b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-17 15:59:53 +0000

    reusing action on single show route

--- routes/shows/routes.php
+++ routes/shows/routes.php
@@ -1,7 +1,16 @@
 <?php
 
 use App\Actions\Podcast\ShowGetOneBySlug;
+use App\Actions\Podcast\ShowGetPodcasts;
 
 $api->get('/shows/{slug}', function ($request, $response, $args) {
     return (new ShowGetOneBySlug($request, $response, $args))->execute();
+});
+
+$api->get('/shows/{showId}/podcasts', function ($request, $response, $args) {
+    return (new ShowGetPodcasts(
+        $request,
+        $response,
+        $args
+    ))->execute();
 });
