e602ed1fdc55c4e7141a0c71e4771e3fa892f67e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-15 21:03:21 +0000

    facebook login step now working

--- Actions/User/Auth/Facebook/Login.php
+++ Actions/User/Auth/Facebook/Login.php
@@ -5,7 +5,6 @@ namespace App\Actions\User\Auth\Facebook;
 use App\Lib\Helpers\Config;
 use App\Lib\Slime\RestAction\ApiAction;
 use OAuth\Common\Consumer\Credentials;
-use OAuth\Common\Http\Uri\UriFactory;
 use OAuth\Common\Storage\Memory;
 use OAuth\ServiceFactory;
 
@@ -13,8 +12,6 @@ class Login extends ApiAction
 {
     protected function performAction()
     {
-        $uriFactory = new UriFactory();
-        $currentUri = $uriFactory->createFromSuperGlobalArray($_SERVER);
         $serviceFactory = new ServiceFactory();
         $facebookConfig = Config::get('authProvider.facebook');
         // Session storage
@@ -23,11 +20,10 @@ class Login extends ApiAction
         $credentials = new Credentials(
             $facebookConfig['id'],
             $facebookConfig['secret'],
-            $currentUri->getAbsoluteUri() . '/callback'
+            $this->getQueryParams()['callback']
         );
+
         $facebookService = $serviceFactory->createService('facebook', $credentials, $storage, ['email']);
-        $url = $facebookService->getAuthorizationUri()->getAbsoluteUri();
-        echo $url;
-        exit;
+        $this->payload = $facebookService->getAuthorizationUri()->getAbsoluteUri();
     }
 }
