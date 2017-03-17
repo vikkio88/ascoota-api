2c666b2b4437aff0d7bff595a6807c506b40a6b9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-17 20:16:46 +0000

    new social provider action and integration with fb #14

--- routes/auth/routes.php
+++ routes/auth/routes.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Actions\User\Auth\Facebook\SignIn as FacebookSignIn;
 use App\Actions\User\Auth\LoginToken;
+
+$api->post('/auth/facebook', function ($request, $response, $args) {
+    return (new FacebookSignIn($request, $response, $args))->execute();
+});
 
 $api->get('/auth/{token}', function ($request, $response, $args) {
     return (new LoginToken($request, $response, $args))->execute();
