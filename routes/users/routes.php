0a85109abcf7417afa220f81907d1832e13ea100
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-18 00:01:04 +0000

    small refactory

--- routes/users/routes.php
+++ routes/users/routes.php
@@ -1,36 +1,12 @@
 <?php
 use App\Actions\User\LoggedIn\MyInfoGet;
 use App\Actions\User\LoggedIn\MyPositionSave;
-use App\Actions\User\UserGetAll;
-
-
-$api->get('/users', function ($request, $response, $args) {
-    return (
-    new UserGetAll(
-        $request,
-        $response,
-        $args
-    )
-    )->execute();
-});
 
 $api->get('/me', function ($request, $response, $args) {
-    return (
-    new MyInfoGet(
-        $request,
-        $response,
-        $args
-    )
-    )->execute();
+    return (new MyInfoGet($request, $response, $args))->execute();
 });
 
 $api->put('/me/position', function ($request, $response, $args) {
-    return (
-    new MyPositionSave(
-        $request,
-        $response,
-        $args
-    )
-    )->execute();
+    return (new MyPositionSave($request, $response, $args))->execute();
 });
 
