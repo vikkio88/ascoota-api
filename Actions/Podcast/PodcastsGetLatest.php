5da260306c049fb73bff8d858ffddcaa6b4c3f7c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 13:01:03 +0000

    adding pagination on latest podcasts on trend endpoint

--- Actions/Podcast/PodcastsGetLatest.php
+++ Actions/Podcast/PodcastsGetLatest.php
@@ -3,13 +3,16 @@
 namespace App\Actions\Podcast;
 
 use App\Lib\Slime\RestAction\ApiAction;
+use App\Lib\Slime\RestAction\Traits\Pagination;
 use App\Models\Podcasts\Podcast;
 
 class PodcastsGetLatest extends ApiAction
 {
+    use Pagination;
     protected function performAction()
     {
-        $this->payload = Podcast::limit(5)
+        $this->pagination = $this->getPaginationParams($this->request);
+        $this->payload = Podcast::page($this->pagination)
             ->with('show')
             ->orderBy('created_at', 'DESC')
             ->get();
