2e9d43fdf1b11fea0a995bccf18c80d29e887d4f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 12:58:55 +0000

    adding pagination on trends show endpoint

--- Actions/Podcast/ShowGetTrends.php
+++ Actions/Podcast/ShowGetTrends.php
@@ -3,12 +3,19 @@
 namespace App\Actions\Podcast;
 
 use App\Lib\Slime\RestAction\ApiAction;
+use App\Lib\Slime\RestAction\Traits\Pagination;
 use App\Models\Podcasts\RadioShow;
 
 class ShowGetTrends extends ApiAction
 {
+    use Pagination;
+
     protected function performAction()
     {
-        $this->payload = RadioShow::orderBy('created_at', 'DESC')->limit(6)->get();
+        $this->pagination = $this->getPaginationParams($this->request);
+        $this->payload = RadioShow::with('language')
+            ->page($this->pagination)
+            ->orderBy('created_at', 'DESC')
+            ->get();
     }
 }
